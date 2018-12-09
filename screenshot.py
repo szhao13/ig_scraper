1e0923da70ec70ab8dc56ba6dd913803003645b0
Shelley Zhao <shelleyzhao@Shelleys-MacBook-Pro.local>
2018-12-09 15:11:24 -0500

    uh shit's broken because of os update

--- screenshot.py
+++ screenshot.py
@@ -1,4 +1,7 @@
 import pyautogui
+import os
+os.chdir(r'/Users/shelleyzhao/interview_prep/ig_scraper')
+# pyautogui.screenshot(r'/Users/shelleyzhao/interview_prep/ig_scraper')
 import time
 
 pyautogui.PAUSE = 1
@@ -7,9 +10,9 @@ pyautogui.FAILSAFE = True
 pyautogui.moveTo(100, 150)
 
 # time.sleep(1)
-im1 = pyautogui.screenshot(region=(0, 0, 900, 1800))
-im1.save('my_screenshot.png')
+# im1 = pyautogui.screenshot(region=(0, 0, 900, 1800))
+# im1.save('my_screenshot.png')
 # im2 = pyautogui.screenshot('my_screenshot.png')
-
+im3 = pyautogui.screenshot()
 # time.sleep(1)
 pyautogui.scroll(-100)
