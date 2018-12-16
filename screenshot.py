05a5c81e67348d97bacba0591cc7008048524f3a
Shelley Zhao <shelleyzhao3@gmail.com>
2018-12-15 23:29:48 -0500

    got ocr integrated, hacky way to get around screenshot bug

--- screenshot.py
+++ screenshot.py
@@ -1,8 +1,16 @@
 import pyautogui
 import os
-os.chdir(r'/Users/shelleyzhao/interview_prep/ig_scraper')
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+# os.chdir(r'/Users/shelleyzhao/interview_prep/ig_scraper')
 # pyautogui.screenshot(r'/Users/shelleyzhao/interview_prep/ig_scraper')
 import time
+# cwd = os.getcwd()  # Get the current working directory (cwd)
+# files = os.listdir(cwd)  # Get all the files in that directory
+# print("Files in '%s': %s" % (cwd, files))
 
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
@@ -12,7 +20,13 @@ pyautogui.moveTo(100, 150)
 # time.sleep(1)
 # im1 = pyautogui.screenshot(region=(0, 0, 900, 1800))
 # im1.save('my_screenshot.png')
-# im2 = pyautogui.screenshot('my_screenshot.png')
-im3 = pyautogui.screenshot()
+im2 = pyautogui.screenshot('my_screenshot.png')
+im2_crop = Image.open("my_screenshot.png")
+cropped = im2_crop.crop((0, 0, 800, 12000))
+cropped.show()
+# print(pytesseract.image_to_string(Image.open('my_screenshot.png')))
+print(pytesseract.image_to_string(Image.open('man_crop.png')))
+
+#im3 = pyautogui.screenshot()
 # time.sleep(1)
-pyautogui.scroll(-100)
+pyautogui.scroll(-10000)
