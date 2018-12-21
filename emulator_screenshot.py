2bdc7b92bfdfad59511ba5ee31eeffecef82ee63
Shelley Zhao <shelleyzhao3@gmail.com>
2018-12-20 23:55:51 -0500

    syntax

--- emulator_screenshot.py
+++ emulator_screenshot.py
@@ -5,4 +5,5 @@ from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 device = MonkeyRunner.waitForConnection() 
 
 package = "com.instagram.android"
+
 runComponent = package + "/" + activity
